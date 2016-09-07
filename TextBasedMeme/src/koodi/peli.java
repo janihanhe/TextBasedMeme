e2f9748455759fb7795e1f34f82b4a1e173c0933
Kobta <villemantyla99@hotmail.com>
2016-09-07 13:43:07 +0300

    Teiden 3 ja 4 alut

--- TextBasedMeme/src/koodi/peli.java
+++ TextBasedMeme/src/koodi/peli.java
@@ -31,6 +31,7 @@ package koodi;
 			int eka;
 			String valinta1 = null;
 			String valinta2 = null;
+			String valinta3 = null;
 			System.out.println("------------------------------------------------------------------------------");
 			System.out.println("");
 			System.out.println("");
@@ -147,6 +148,24 @@ package koodi;
 	         if(valinta2.equals("Ei") || valinta2.equals("ei")){
 	         	System.out.println(" -Lähdet seuraamaan aiemmin näkemääsi valoa- ");
 	         	System.out.println(" ");// TIE 1: JOKU LISÄÄ TÄHÄN PASKAA
+	         	System.out.println(" -Matkalla valoa päin, sinut pysäyttää mustiin pukeutunut mies")
+	         	System.out.println("?: Hei kamu, tuuks mukaan? Mennää mettää");
+	         	System.out.println("?: Jos tuut messii, voin pitää sut turvassa");
+	         	System.out.println(" ");
+	         	System.out.println(" Menetkö hänen mukaan? ");
+	         	System.out.println("Kyllä vai Ei");
+	         	valinta3 = lukijas.nextLine();
+	         	
+	         	if(valinta3.equals("Ei") || valinta3.equals("ei")){
+	         		System.out.println(" -Et huomio miestä ja jatkat matkaa");
+	         		System.out.println(" ");
+	         		System.out.println(" -Pääset vihdoin valon tuottajan luokse");
+	         		System.out.println(" -Huomaat, että valon tuottaja oli todella vahva taskulamppu"); //TIE 4: JOKU LISÄÄ TÄHÄN PASKAA
+	         		
+	         	}else if(valinta3.equals("Kyllä") || valinta3.equals("kyllä")){ // TIE 3: JOKU LISÄÄ TÄHÄN PASKAA
+	         		
+	         	}	
+	         	
 	         }else if(valinta2.equals("Kyllä") || valinta2.equals("kyllä")){ // TIE 2: JOKU LISÄÄ TÄHÄN PASKAA
 	        	 System.out.println("Kävelet kujaa päin ja eteesi hyppää villi koira");
 	        	 System.out.println("");
